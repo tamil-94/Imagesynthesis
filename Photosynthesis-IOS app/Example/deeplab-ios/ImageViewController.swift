710c5c97e062b54c7c6dcfaf18240bb4a359bb2d
tamil-94 <56046373+tamil-94@users.noreply.github.com>
2019-11-18 08:40:05 -0500

    Update ImageViewController.swift

--- Photosynthesis-IOS app/Example/deeplab-ios/ImageViewController.swift
+++ Photosynthesis-IOS app/Example/deeplab-ios/ImageViewController.swift
@@ -9,23 +9,17 @@ class ImageViewController: UIViewController {
     var testuiimage: UIImage!
     var bgImg: UIImage!
     var fgImg:UIImage!
-   
-    var imagePicked = 0
-     let imagePicker =  UIImagePickerController()
     var sourceImg: UIImage!
     
     var sourceImg1: UIImage!
     var fo: Int!
     var ro: Int!
-   // var cgImg: CGImage!
-    //var uiimtest: UIImage!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         displayView.image = sourceImg
         testview.image = sourceImg1
       
-        imagePicker.delegate = self as UIImagePickerControllerDelegate & UINavigationControllerDelegate
-        imagePicker.sourceType = .camera
         displayView.layer.borderColor = UIColor.white.cgColor
         testview.layer.borderColor = UIColor.white.cgColor
             
@@ -42,28 +36,7 @@ class ImageViewController: UIViewController {
  
     }
 
-    @IBAction func handlePickerTap(_ sender: Any) {
-        let imagePicker =  UIImagePickerController()
-        imagePicker.delegate = self
-        present(imagePicker, animated: true, completion: nil)
-    }
-    @IBAction func handlePhotoTap(_ sender: UIButton) {
-        //let imagePicker =  UIImagePickerController()
-        //imagePicker.delegate = self
-        //imagePicker.sourceType = .camera
-        sender.isHidden = true;
-        imagePicked = sender.tag
-        
-        present(imagePicker, animated: true, completion: nil)
-    }
-    
-    
-    @IBAction func handleSegmentTap(_ sender: Any) {
-        
-        if let cgImg = sourceImg.segmentation(){
-            displayView.image = UIImage(cgImage: cgImg)
-        }
-    }
+  
     @IBAction func handelMergeTap(_ sender: Any) {
         
         if let cgImg = sourceImg.segmentation(){
@@ -103,68 +76,6 @@ class ImageViewController: UIViewController {
             
         }
     }
-    
-    
-    @IBAction func handelGrayTap(_ sender: Any) {
-        if let cgImg = sourceImg.segmentation(){
-            let filter = GraySegmentFilter()
-            filter.inputImage = CIImage.init(cgImage: sourceImg.cgImage!)
-            filter.maskImage = CIImage.init(cgImage: cgImg)
-            let output = filter.value(forKey:kCIOutputImageKey) as! CIImage
-            
-            let ciContext = CIContext(options: nil)
-            let cgImage = ciContext.createCGImage(output, from: output.extent)!
-            displayView.image = UIImage(cgImage: cgImage)
-        }
-    }
-    
-    
+
     
 }
-
-extension ImageViewController: UIImagePickerControllerDelegate,UINavigationControllerDelegate {
- 
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            
-            if imagePicked == 1 {
-            
-                sourceImg = pickedImage.resize(size: CGSize(width: 1200, height: 1200 * (pickedImage.size.height / pickedImage.size.width)))
-                
-            }
-            else if imagePicked == 2 {
-                sourceImg1 = pickedImage.resize(size: CGSize(width: 1200, height: 1200 * (pickedImage.size.height / pickedImage.size.width)))
-                
-            }
-            dismiss(animated: true)
-            }
-            func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-                dismiss(animated: true)
-            }
-        }
-        
- 
-    
-    }
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
